c73bc45a073fe98d3935b0609b2663c1ebb37f50
Shayne Quinton <shayne_quinton@hotmail.com>
2021-10-05 22:31:30 -0400

    UPDATE: Beat Number background is more transparent
    The alpha on the background material for the beat numbers has been set to be more transparent.

--- Assets/_Project/Scripts/InvisibleMask.cs
+++ Assets/_Project/Scripts/InvisibleMask.cs
@@ -10,24 +10,16 @@ public class InvisibleMask : MonoBehaviour
     [SerializeField]
     List<GameObject> objectsToMask = new List<GameObject>();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
         
         var renders = objectsToMask.SelectMany(obj => obj.GetComponentsInChildren<Renderer>().ToList());
-
-
         
  
         foreach (Renderer rendr in renders)
         {
-            rendr.material.renderQueue = 3052; // set their renderQueue
+            rendr.material.renderQueue = 3052; // this render queue must be after the Invisible Mask shader render queue.
         }
 
         var textRenderers = objectsToMask.SelectMany(obj=>GetComponentsInChildren<TMP_Text>().ToList());
