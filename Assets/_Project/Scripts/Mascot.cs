aad8ff9c14dd88a886d502f57bf56f67cd924e40
Shayne Quinton <shayne_quinton@hotmail.com>
2021-10-26 20:18:15 -0400

    FEATURE: Added Particles to the Mascot.

--- Assets/_Project/Scripts/Mascot.cs
+++ Assets/_Project/Scripts/Mascot.cs
@@ -4,27 +4,37 @@ using UnityEngine;
 
 public class Mascot : MonoBehaviour
 {
-
     [SerializeField]
-    public float songPositionInBeats= 0.0f;
-
+    private ParticleSystem _particleSystem;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Conductor.Instance.OnBeat += (info) =>
+         {
+             if (info.CurrentBeatIn4x4Time == 0)
+             EmitParticles();
+         };
     }
 
     // Update is called once per frame
     void Update()
     {
-        float signedPulse = Mathf.Cos(songPositionInBeats * Mathf.PI) % Mathf.PI;
-        float pulseValue = Mathf.Abs(signedPulse);
-        pulseValue = EaseOutSine(pulseValue);
+        
+        float signedPulse = Mathf.Cos(Conductor.Instance.SongPositionInBeats * Mathf.PI) % Mathf.PI; //what is this?
+        float pulseValue = Mathf.Abs(signedPulse); //what is this?
+        pulseValue = EaseOutSine(pulseValue); //what is this? (easing functoion)
+        
+
         float scaleValue = (pulseValue * 0.5f) + 2;
         transform.localScale = new Vector3(scaleValue,scaleValue,scaleValue);
 
         transform.eulerAngles = new Vector3(0, 0 , signedPulse * 25);
+    }
+
+    private void EmitParticles()
+    {
+        _particleSystem.Play();
     }
 
     private float EaseOutSine(float x)
