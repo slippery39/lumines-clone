1be8ca4695c3368e943290a5aa976ae32dce0cfb
Shayne <shayne_quinton@hotmail.com>
2022-02-03 22:43:44 -0500

    spiced up the main menu, added glowing text
    spiced up the main menu, added glowing text

---
+++ Assets/_Project/Unity Scripts/ApplicationManagement/GlowingText.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GlowingText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // References
+    public TextMeshProUGUI text;
+    private static Material textBaseMaterial;
+    private static Material textHighlightMaterial;
+
+    private void Awake()
+    {
+        // Get a reference to the default base material
+        textBaseMaterial = text.fontSharedMaterial;
+
+        // Create new instance of the material assigned to the text object
+        // Assumes all text objects will use the same highlight
+        textHighlightMaterial = new Material(textBaseMaterial);
+
+        // Set Glow Power on the new material instance
+        textHighlightMaterial.SetFloat(ShaderUtilities.ID_GlowPower, 0.0f);
+        text.fontSharedMaterial= textHighlightMaterial;
+    }
+
+    private void Update()
+    {
+        textHighlightMaterial.SetFloat(ShaderUtilities.ID_GlowPower, Mathf.Abs(Mathf.Sin(Time.fixedUnscaledTime * Mathf.PI)));
+        text.UpdateMeshPadding();
+    }
+
+}
---
+++ Assets/_Project/Unity Scripts/ApplicationManagement/GlowingTextOnMouse.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GlowingTextOnMouse : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    // References
+    public TextMeshProUGUI text;
+    private static Material textBaseMaterial;
+    private static Material textHighlightMaterial;
+    private bool isInText = false;
+
+    private void Awake()
+    {
+        // Get a reference to the default base material
+        textBaseMaterial = text.fontSharedMaterial;
+
+        // Create new instance of the material assigned to the text object
+        // Assumes all text objects will use the same highlight
+        textHighlightMaterial = new Material(textBaseMaterial);
+
+        // Set Glow Power on the new material instance
+        textHighlightMaterial.SetFloat(ShaderUtilities.ID_GlowPower, 0.0f);
+    }
+
+    void Update()
+    {
+        if (isInText)
+        {
+            textHighlightMaterial.SetFloat(ShaderUtilities.ID_GlowPower, Mathf.Abs(Mathf.Sin(Time.fixedUnscaledTime * (Mathf.PI*2))));
+            text.UpdateMeshPadding();
+        }
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isInText = true;
+        text.fontSharedMaterial = textHighlightMaterial;
+        text.UpdateMeshPadding();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.fontSharedMaterial = textBaseMaterial;
+    }
+}
