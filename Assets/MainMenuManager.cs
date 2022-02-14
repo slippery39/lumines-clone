f21f8c17c052b2559d7b21827b24785535b18064
Shayne <shayne_quinton@hotmail.com>
2022-02-13 23:18:45 -0500

    added in new skin

--- Assets/MainMenuManager.cs
+++ Assets/MainMenuManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
+
+    public GameObject mainMenu;
+    public GameObject howToPlay;
+
     // Start is called before the first frame update
     void Start()
     {
